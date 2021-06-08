0f80b4aa3c4e3bbb57b31324864a16a02a91ac86
cohako <muri.ilo@hotmail.com>
2021-06-07 21:55:07 -0300

    Creating Users models with enum

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  enum roles: {client_user: 0, client_admin: 5, admin: 777 }
+end
