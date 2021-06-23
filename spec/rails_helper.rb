ab17c5fc5e675c21b80e9914873149b20400e7a5
cohako <muri.ilo@hotmail.com>
2021-06-23 19:51:20 -0300

    Add some config for simplecov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,10 +1,17 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 require 'capybara/rspec'
-require 'simplecov'
-SimpleCov.start
+
 
 ENV['RAILS_ENV'] ||= 'test'
+if ENV['RAILS_ENV'] == 'test'
+  require 'simplecov'
+  SimpleCov.start 'rails' do
+  add_filter "/app/jobs"
+  add_filter "/app/mailers"
+  puts "required simplecov"
+  end
+end
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
